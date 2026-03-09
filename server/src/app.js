49d242a43cd85cf4fbba2e38802f619b21e006f9
LAKSHYA CHOUDHARY <choudharylakshya.lc@gmail.com>
2026-03-09 16:13:43 +0530

    p

--- server/src/app.js
+++ server/src/app.js
@@ -49,11 +49,14 @@ app.use(passport.initialize())
 app.use(passport.session());
 app.use("/api",mainRouter)
 
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
 
+// Serve index.html for all other GET requests (React Router support)
+app.get(/.*/,(req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "dist", "index.html"));
+});
 
-app.use((req,res)=>{
-     res.sendFile(path.join(__dirname, "..", "..", "client", "dist", "index.html"))
-})
 
 
 
