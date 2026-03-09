3387bd137035ca5d7801ad99d20431d6888e57b3
LAKSHYA CHOUDHARY <choudharylakshya.lc@gmail.com>
2026-03-09 13:39:05 +0530

    production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(),tailwindcss()],
+  base: "./",
 })
