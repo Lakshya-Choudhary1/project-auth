3db3279cd29a365a79f494a303c0b84692f7ca57
LAKSHYA CHOUDHARY <choudharylakshya.lc@gmail.com>
2026-03-09 14:46:40 +0530

    p

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='w-screen h-screen bg-amber-50 flex justify-center items-center'>
       <h1 className='text-3xl text-blue-700'>authproject</h1>
+      <a href="/api/user/oauth/google" className="text-blue-500 hover:underline">
+        Login with Google
+      </a>
     </div>
   )
 }
